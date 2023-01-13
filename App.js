59eb08899692b8fca92d51ea257c7079acf1d3ce
aguilarklyno <aguilarklyno@gmail.com>
2023-01-13 12:13:26 +0900

    setups

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
+import React from "react";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+
+import All from "./screen"
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="All" component={All} />
+        <Tab.Screen name="Business" component={Business} />
+        <Tab.Screen name="Health" component={HealthScreen} />
+        <Tab.Screen name="Sports" component={SportsScreen} />
+        <Tab.Screen name="Tach" component={TechScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your a</Text>
       <StatusBar style="auto" />
     </View>
   );
