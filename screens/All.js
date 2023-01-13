dac6de1fc2ac9b5d0b8751d6d603d2f9634d8b93
aguilarklyno <aguilarklyno@gmail.com>
2023-01-13 22:56:31 +0900

    complete

--- screens/All.js
+++ screens/All.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState} from 'react'
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 import { NativeBaseProvider, FlatList, ScrollView, Divider, Image, Spinner } from 'native-base';
 import { services } from '../services/Services';
+import moment from 'moment/moment';
+// import { styles } from 'styled-system';
 
 export default function All() {
   const [newsData, setNewsData] = useState([])
+
   useEffect(() => {
     services('general')
     .then(data => {
@@ -14,30 +17,70 @@ export default function All() {
       alert(error)
     })
   }, [])
+
   return (
     <NativeBaseProvider>
       <ScrollView height={850}>
+        {newsData.length > 1 ?(
         <FlatList
           data={newsData}
           renderItem={({ item }) => (
           <View>
-            <View>
-              <Text>
+            <View style={styles.newsContainer}>
+              <Image
+                width={550}
+                height={250}
+                resizeMode={"cover"}
+                source={{
+                  uri: item.urlToImage,
+                }}
+                alt="Alternate Text"
+              />
+              <Text style={styles.title}>
                 {item.title}
               </Text>
-              <Text>
-                {item.publishedAt}
+              <Text style={styles.date}>
+                {moment(item.publishedAt).format('LLL')}
               </Text>
-              <Text>
+              <Text style={styles.newsDescription}>
                 {item.description}
               </Text>
             </View>
+            <Divider my={2} bg="#e0e0e0"/>
           </View> 
           )}
           keyExtractor={(item) => item.id}
         />
+        ) : (
+          <View style={styles.spinner}>
+            <Spinner color="denger.400" />
+          </View>
+        )}
       </ScrollView>
     </NativeBaseProvider>
     )
 }
 
+const styles = StyleSheet.create({
+  newsContainer: {
+    padding: 10
+  },
+  title: {
+    fontSize: 18,
+    marginTop: 10,
+    fontWeight: "600",
+  },
+  newsDescription: {
+    fontSize: 16,
+    marginTop: 10,
+  },
+  date: {
+    fontSize: 14,
+  },
+  spinner: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 400,
+  }
+})
